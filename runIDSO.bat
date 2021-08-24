6f6a6b9b89c879e0a2a8deb4f76a140b369a1eae
Swathi <battulaswathi@gmail.com>
2021-08-25 00:29:54 +0530

    Add files via upload

--- runIDSO.bat
+++ runIDSO.bat
@@ -6,13 +6,12 @@ set JsonFilesDir=%InputFilesDir%\JsonFiles
 set OutputFilesDir=%DSDirectory%\OutputFiles
 set LogFilesDir=%OutputFilesDir%\LogFiles
 set PlotFilesDir=%OutputFilesDir%\PlotFiles
-set GLDOutcomesDir=%OutputFilesDir%\GLDOutcomes
 
 
 set "NDay=2"
 set "NHour=4"
 set "deltaT=300"
-set "NoOfHouses=927"
+set "NoOfHouses=4"
 set "NDistSys=1"
 set "DistFeederFileName=IEEE123Feeder"
 set /a "tmax=%NDay%*86400+%NHour%*3600"
@@ -29,7 +28,6 @@ set "RefLoad=1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 15
 md %OutputFilesDir% 2> nul
 md %LogFilesDir% 2> nul
 md %PlotFilesDir% 2> nul
-md %GLDOutcomesDir% 2> nul
 
 set FNCS_FATAL=no
 set FNCS_LOG_STDOUT=yes
@@ -47,8 +45,8 @@ set FNCS_LOG_LEVEL=DEBUG2
 FOR /L %%i IN (1,1,%NDistSys%) DO start /b cmd /c gridlabd %InputFilesDir%/%DistFeederFileName%Modified%%i.glm ^>%LogFilesDir%/gridlabd%%i.log 2^>^&1
 
 set FNCS_LOG_LEVEL=
-runHouseholds927.bat
-REM start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_1_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_1.log 2^>^&1
-REM start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_2_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_2.log 2^>^&1
-REM start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_3_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_3.log 2^>^&1
-REM start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_4_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_4.log 2^>^&1
+REM runHouseholds927.bat
+start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_1_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_1.log 2^>^&1
+start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_2_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_2.log 2^>^&1
+start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_3_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_3.log 2^>^&1
+start /b cmd /c python ./House/HouseController.py ./InputFiles/JsonFiles/controller_registration_house_1A_4_thermostat_controller.json %tmax% %deltaT% ^>%logfilesdir%/house_1A_4.log 2^>^&1

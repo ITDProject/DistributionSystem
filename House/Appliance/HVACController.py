61326787efe6272f9f84780643e481bd30329f77
Swathi <battulaswathi@gmail.com>
2021-08-24 23:19:26 +0530

    Add files via upload

--- House/Appliance/HVACController.py
+++ House/Appliance/HVACController.py
@@ -9,7 +9,7 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-from FNCS import fncs
+import fncs
 
 # Class definition
 class HVACController:
